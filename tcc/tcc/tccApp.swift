e9366d6046231f301c55b483bfd9eeb7042c8e41
Rafael Dias Gonçalves <rafael.goncalves@compasso.com.br>
2025-11-13 13:25:27 -0400

    projeto xcode subido

---
+++ tcc/tcc/tccApp.swift
@@ -0,0 +1,17 @@
+//
+//  tccApp.swift
+//  tcc
+//
+//  Created by Rafael Dias Gonçalves on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct tccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
